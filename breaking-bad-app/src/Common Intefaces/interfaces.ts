bb8075eea31edb5e21c42d533ae73931cb9ebf49
mathewseto1 <48811930+mathewseto1@users.noreply.github.com>
2020-07-13 02:27:27 +1200

    Created the search and the submit button
    created a component for the search bar which uses the textfield and the button from React material UI

---
+++ breaking-bad-app/src/Common Intefaces/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IUserInput {
+    userInput : (string | null);
+}
---
+++ breaking-bad-app/src/Components/SearchBar.tsx
@@ -0,0 +1,50 @@
+import React,{useState} from 'react';
+import Button from '@material-ui/core/Button'
+import { Grid, TextField } from '@material-ui/core';
+import {IUserInput} from '../Common Intefaces/interfaces';
+import { prependOnceListener } from 'process';
+
+interface ISearchBarProps {
+ SetUserInput: (userInput: IUserInput) => void; //the interface is a function that takes a user input and returns nothing.
+}
+
+function SearchBar() {
+    const [UserInput, SetUserInput] = useState<string | null>("");
+
+    const handleUserInputChange =  (UserStringValue : string |null) =>{
+        SetUserInput(UserStringValue);
+    }
+
+    const handleSubmit = () => {
+
+        if (UserInput!== null && UserInput !== "" && UserInput.length !== 0){
+            let userInput : IUserInput ={
+                userInput:UserInput
+            }
+        // console.log(userInput)
+        //props.SetUserInput(userInput)
+        }
+        else{
+            setFocusOn(true);
+        }
+    }
+
+    const [FocusOn, setFocusOn] = useState<boolean>(false);
+
+    return (
+    <div>
+        <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+                <TextField id="outlined-basic" label="Enter a Character" variant="outlined" onClick = {() => setFocusOn(true)} error ={FocusOn && UserInput === ""} value = {UserInput} onChange ={text => handleUserInputChange(text.target.value)}/>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Button variant="contained" color="primary" onClick = {handleSubmit}>Submit</Button>
+            </Grid>
+        
+        </Grid>
+
+    </div>
+    )
+}
+
+export default SearchBar
