2c88cd7bca32b604375ab15935a5e35f9b2b7ce6
mathewseto1 <48811930+mathewseto1@users.noreply.github.com>
2020-07-13 15:46:52 +1200

    character card component
    created the basic character card component with the image to be displayed and text.

--- breaking-bad-app/src/App.tsx
+++ breaking-bad-app/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Components/SearchBar';
+import CharacterCards from './Components/CharacterCards'
 
 function App() {
   return (
     <div className="App">
       <p>hello there</p>
       <SearchBar/>
+      <CharacterCards/>
     </div>
   );
 }
--- breaking-bad-app/src/Components/CharacterCards.tsx
+++ breaking-bad-app/src/Components/CharacterCards.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
+interface ICharacterCardProps{
+    image: string | undefined,
+    characterInformation:string | undefined
+}
+
+// export default function CharacterCards(props:ICharacterCardProps) {
 export default function CharacterCards() {
     return (
-        <div>
-            
-        </div>
+        <Card className="MediaCardContainer">
+            <CardActionArea>
+            <CardMedia image = "./FILL IN LATER"
+            style={{height: 20, paddingTop: '20%'}}
+            title="Place holder image"
+            />
+            <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+            </CardActionArea>
+        </Card>
     )
 }
