cdfadac6b3a476a9892b9240b690c7f33ddca1a1
mathewseto1 <48811930+mathewseto1@users.noreply.github.com>
2020-07-23 21:03:01 +1200

    developing a expanding section

--- breaking-bad-app/src/Components/CharacterCards.tsx
+++ breaking-bad-app/src/Components/CharacterCards.tsx
@@ -5,10 +5,16 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import '../Styles/Style.css'
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import Collapse from '@material-ui/core/Collapse';
 
 interface ICharacterCardProps{
     image: string | undefined,
@@ -16,8 +22,32 @@ interface ICharacterCardProps{
     characterInformation: String[],
 }
 
+const useStyles = makeStyles((theme) => ({
+
+    media: {
+      height: 0,
+      paddingTop: '56.25%',
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',}
+    }));
+
 
 export default function CharacterCards(props:ICharacterCardProps) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
     return (
         <Card className="MediaCardContainer">
             <CardActionArea>
@@ -46,6 +76,29 @@ export default function CharacterCards(props:ICharacterCardProps) {
             </List>
           </div>
         </CardContent>
+        <div>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+        </div>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Have not implemented any features for the expanding icon yet.
+          <iframe src="https://giphy.com/embed/sIIhZliB2McAo" title = "nyan cat" width="120" height="100" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/nyan-cat-sIIhZliB2McAo">via GIPHY</a></p>
+          </Typography>
+
+        </CardContent>
+        </Collapse>   
+            
             </CardActionArea>
         </Card>
     )
